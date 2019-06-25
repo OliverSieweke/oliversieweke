efebef6e1dc406dca31f3cd69c9f797a385344a9
Oliver Sieweke <oliver.sieweke@protonmail.com>
2019-06-25 16:03:08 +0200

    Disable Programming Notes.

--- src/templates/programming-notes.js
+++ src/templates/programming-notes.js
@@ -4,20 +4,23 @@ import { graphql } from "gatsby";
 
 const ProgrammingNotes = ({ data: { markdownRemark: { html, frontmatter: { title } } } }) => (
     <React.Fragment>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div>
+            Programming Note
+        </div>
+        {/*<h1>{title}</h1>*/}
+        {/*<div dangerouslySetInnerHTML={{ __html: html }} />*/}
     </React.Fragment>
 );
 
 export default ProgrammingNotes;
 
-export const query = graphql`
-    query ProgrammingNotesQuery($slug: String!) {
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-                title
-            }
-        }
-    }
-`;
+// export const query = graphql`
+//     query ProgrammingNotesQuery($slug: String!) {
+//         markdownRemark(fields: { slug: { eq: $slug } }) {
+//             html
+//             frontmatter {
+//                 title
+//             }
+//         }
+//     }
+// `;
