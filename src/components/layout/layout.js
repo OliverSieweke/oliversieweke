70a7d7f46bcb1df55f61e9c5a715d447bdfe1b36
OliverSieweke <oliver@sieweke.eu>
2020-03-05 12:07:58 +0100

    Include layout only for navigation items.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,6 +9,7 @@ import styles              from "../../styles/layout.module.css";
 // Fonts •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 import "typeface-merriweather";
 import "typeface-lato";
+import { useSiteMetaData } from "../../static-queries/use-site-metadata.js";
 
 // ================================================================================================================== \\
 // ================================================ LAYOUT COMPONENT ================================================ \\
@@ -17,22 +18,29 @@ export const Layout = ({ children, location }) => {
     // A slightly different layout is required for the Javascript notes with TOC.
     const [toc, setToc] = useState();
     const [tocVisible, setTocVisible] = useState();
+    const { navigationItems } = useSiteMetaData();
+    const [firstPathElement] = location.pathname.match(/^\/?[^/]+/u);
 
-    return (
-        <div className={styles.layout}>
-            <GlobalSEO/>
-            <Header {...{ toc, tocVisible, setTocVisible }}/>
-            <div className={styles.page}>
-                {tocVisible && <JavaScriptToc {...{ toc, setTocVisible, location }}/>}
-                <div className={styles.contentContainer}>
-                    <div className={`${styles.content} ${toc ? styles.javascriptContent : ""}`}>
-                        <main className={styles.main}>
-                            {React.cloneElement(children, { setToc, setTocVisible })}
-                        </main>
-                        <Footer/>
+    if (navigationItems.find(navigationItem => new RegExp(`^${firstPathElement}/$`, "u").test(navigationItem.link))) {
+        return (
+            <div className={styles.layout}>
+                <GlobalSEO/>
+                <Header {...{ toc, tocVisible, setTocVisible }}/>
+                <div className={styles.page}>
+                    {tocVisible && <JavaScriptToc {...{ toc, setTocVisible, location }}/>}
+                    <div className={styles.contentContainer}>
+                        <div className={`${styles.content} ${toc ? styles.javascriptContent : ""}`}>
+                            <main className={styles.main}>
+                                {React.cloneElement(children, { setToc, setTocVisible })}
+                            </main>
+                            <Footer/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return children;
+    }
+
 };
