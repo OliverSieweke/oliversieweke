c3cc918739fd58e81de2000e74121bb34543597e
OliverSieweke <oliver@sieweke.eu>
2020-10-25 22:20:26 +0100

    Set page to "Under Construction"

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-import { GlobalSEO }       from "../seo/global-seo.js";
-import { Header }          from "./header.js";
-import { JavaScriptToc }   from "../javascript/javascript-toc.js";
-import { Footer }          from "./footer.js";
+import React         from "react";
+import { GlobalSEO } from "../seo/global-seo.js";
 // Styles ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 import "../../styles/global.css";
-import styles              from "../../styles/layout.module.css";
+import styles        from "../../styles/layout.module.css";
 // Fonts •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 import "typeface-merriweather";
 import "typeface-lato";
@@ -13,26 +10,12 @@ import "typeface-lato";
 // ================================================================================================================== \\
 // ================================================ LAYOUT COMPONENT ================================================ \\
 
-export const Layout = ({ children, location }) => {
-    // A slightly different layout is required for the Javascript notes with TOC.
-    const [toc, setToc] = useState();
-    const [tocVisible, setTocVisible] = useState();
-
-    return (
-        <div className={styles.layout}>
-            <GlobalSEO/>
-            <Header {...{ toc, tocVisible, setTocVisible }}/>
-            <div className={styles.page}>
-                {tocVisible && <JavaScriptToc {...{ toc, setTocVisible, location }}/>}
-                <div className={styles.contentContainer}>
-                    <div className={`${styles.content} ${toc ? styles.javascriptContent : ""}`}>
-                        <main className={styles.main}>
-                            {React.cloneElement(children, { setToc, setTocVisible })}
-                        </main>
-                        <Footer/>
-                    </div>
-                </div>
-            </div>
+export const Layout = ({ children }) => (<React.Fragment>
+        <GlobalSEO/>
+        <div className={styles.page}>
+            <main className={styles.main}>
+                {React.cloneElement(children)}
+            </main>
         </div>
-    );
-};
+    </React.Fragment>
+);
