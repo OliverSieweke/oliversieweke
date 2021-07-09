c0e657fef9db43e6d5124faab840dc1fe6e0c984
OliverSieweke <oliver@sieweke.eu>
2021-07-09 20:21:22 +0200

    Update!

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,55 @@
+const cachesDiv = document.getElementById("caches");
+const addButton = document.getElementById("add");
+const updateButton = document.getElementById("update");
+
+function cacheHtml(position) {
+  console.log("position:\t", position);
+
+  return `
+<div class="cache">
+<input name="title" placeholder="Title">
+<input name="latitude" placeholder="Latitude"  value="${position.coords.latitude}" type="number"/>
+<input name="longitude" placeholder="Longitude" value="${position.coords.longitude}" type="number" />
+<input name="description" placeholder="Description"/>
+      <button class="remove">-</button>
+
+</div>`;
+}
+
+addButton.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    cachesDiv.innerHTML += cacheHtml(position);
+
+    const removeButtons = document.getElementsByClassName("remove");
+    for (const removeButton of Array.from(removeButtons)) {
+      console.log("removeButton:\t", removeButton);
+
+      removeButton.addEventListener("click", () => {
+        console.log("asdf");
+        removeButton.parentNode.remove();
+      });
+    }
+  });
+});
+
+updateButton.addEventListener("click", () => {
+  const caches = Array.from(document.getElementsByClassName("cache")).map(
+    (cacheElement) => {
+      const inputs = cacheElement.querySelectorAll("input");
+      const cache = {};
+
+      return Array.from(inputs).reduce((cache, input) => {
+        return {
+          ...cache,
+          [input.name]: input.value,
+        };
+      }, {});
+    }
+  );
+
+  save(caches);
+});
+
 getCaches();
 
 async function getCaches() {
@@ -8,14 +60,36 @@ async function getCaches() {
   const cachesCollection = mongodb.db("caches").collection("caches");
   const caches = await cachesCollection.find();
 
+  console.log("caches:\t", caches);
+
   for (const { latitude, longitude, title, description } of caches) {
-    document.body.innerHTML += `<div>
-<input value="${title}">
-<input type="number" value="${latitude}"/>
-<input type="number" value="${longitude}"/>
-<input value="${description}"/>
-</div>`;
+    cachesDiv.innerHTML += `<div class="cache">
+      <input name="title" value="${title}">
+      <input name="latitude" type="number" value="${latitude}"/>
+      <input name="longitude" type="number" value="${longitude}"/>
+      <input name="description" value="${description}"/>
+      <button class="remove">-</button>
+      </div>
+        `;
+  }
+
+  const removeButtons = document.getElementsByClassName("remove");
+  for (const removeButton of Array.from(removeButtons)) {
+    console.log("removeButton:\t", removeButton);
+
+    removeButton.addEventListener("click", () => {
+      console.log("asdf");
+      removeButton.parentNode.remove();
+    });
   }
 }
 
-function save() {}
+async function save(caches) {
+  const app = new Realm.App({ id: "narayanin-mvhku" });
+  const credentials = Realm.Credentials.anonymous();
+  const user = await app.logIn(credentials);
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const cachesCollection = mongodb.db("caches").collection("caches");
+  await cachesCollection.deleteMany();
+  await cachesCollection.insertMany(caches);
+}
--- public/main.js
+++ public/main.js
@@ -1,34 +1,59 @@
-console.log(L)
+console.log(L);
 
-const map = L.map('map').setView([52.3676, 4.9041], 13);
+const map = L.map("map").setView([52.3676, 4.9041], 13);
 
+map.locate({ watch: true });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+map.on("locationfound", (event) => {
+  var radius = event.accuracy * 2;
+
+  L.circle(event.latlng, radius).addTo(map);
+  console.log("event:\t", event);
+});
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: "mapbox/streets-v11",
     // tileSize: 512,
     // zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoib2xpdmVyc2lld2VrZSIsImEiOiJja24yNm93aHcxNG12Mm90Nzk1aDMzbG01In0.W5z-cZJMwvrdywELIpVy7Q\n'
-}).addTo(map);
+    accessToken:
+      "pk.eyJ1Ijoib2xpdmVyc2lld2VrZSIsImEiOiJja24yNm93aHcxNG12Mm90Nzk1aDMzbG01In0.W5z-cZJMwvrdywELIpVy7Q\n",
+  }
+).addTo(map);
 
-
-getCaches()
+getCaches();
 
 async function getCaches() {
-    const app = new Realm.App({ id: "narayanin-mvhku"});
-    const credentials = Realm.Credentials.anonymous();
-    const user = await app.logIn(credentials);
-    const mongodb = app.currentUser.mongoClient("mongodb-atlas");
-    const cachesCollection = mongodb.db("caches").collection("caches");
-    const caches = await cachesCollection.find()
+  const app = new Realm.App({ id: "narayanin-mvhku" });
+  const credentials = Realm.Credentials.anonymous();
+  const user = await app.logIn(credentials);
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const cachesCollection = mongodb.db("caches").collection("caches");
+  const caches = await cachesCollection.find();
 
-    for(const [index, {latitude, longitude, title, description}] of caches.entries()) {
+  console.log("caches:\t", caches);
 
-        const marker = L.marker([latitude, longitude]).addTo(map)
-        marker.bindPopup(`<span style="font-weight: bold;">${title}<span><br><p>${description}</p>`)
-        if(index === 0) {
-            marker.openPopup()
-        }
-    }
+  for (const [
+    index,
+    { latitude, longitude, title, description, special },
+  ] of caches.entries()) {
+    const dot = L.icon({
+      iconUrl: "dot.png",
+      iconSize: [20, 20], // size of the icon
+      iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
+    });
+    const marker = L.marker([latitude, longitude], {
+      ...(special ? { icon: dot } : {}),
+    }).addTo(map);
+    marker.bindPopup(
+      `<span style="font-weight: bold;">${title}<span><br><p>${description}</p>`
+    );
+    // if(index === 0) {
+    //     marker.openPopup()
+    // }
+  }
 }
