4a20cbafcc86564a9e9fc411e5976e915e136a79
OliverSieweke <oliver@sieweke.eu>
2022-03-01 16:36:05 +0100

    Update online date

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Index({ location }) {
                 <h1>🛠 Under Construction 🛠</h1>
                 <p>
                     {/* eslint-disable-next-line max-len */}
-                    Thanks for dropping by! I&apos;ve had troubles keeping the content of this website up to date. I have taken things down temporarily and am hoping to come back online in 2021.
+                    Thanks for dropping by! I&apos;ve had troubles keeping the content of this website up to date. I have taken things down temporarily and am hoping to come back online in 2022.
                 </p>
                 <p>
                     If you wish, you may take this as an opportunity to reflect on life instead...
